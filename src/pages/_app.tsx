1279af05ef1ba336a929cac99984dca74ac8421c
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2023-04-03 12:38:23 +0900

    5.2.2　styledComponentの設定完了

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { Component } from "react";
 import { createGlobalStyle } from "styled-components";
 
 // グローバルのスタイル
@@ -29,13 +30,19 @@ ol, ul {
 }
 `
 
-const MyApp = ({ Comment, pageProps }: AppProps) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
         <meta key="charset" name="charset" content="utf-8"/>
         <meta key="viewport" name="viewport" content="width-device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"/>
+        <meta property="og:locale" content="ja_JP"/>
+        <meta property="og:type" content="website"/>
       </Head>
+      <GlobalStyle/>
+      <Component {...pageProps}/>
     </>
   )
 }
+
+export default MyApp
